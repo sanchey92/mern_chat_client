7581a7a4784fd9ebb78f7419773b59bc450d0d9d
alexandr <sashkaageev@gmail.com>
2020-08-06 00:43:13 +0300

    added axiosService for server requests

--- src/axiosService.ts
+++ src/axiosService.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default class AxiosService {
 
   public async post(name: string | null, email: string, password: string): Promise<any> {
-    if (!name === null) {
+    if (name !== null) {
       try {
         return await axios.post("http://localhost:8000/user/register", {name, email, password})
       } catch (e) {
